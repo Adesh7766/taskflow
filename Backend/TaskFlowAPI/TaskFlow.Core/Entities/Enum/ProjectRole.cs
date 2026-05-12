cc47a60ed8edb8462f5d7a389f93f0424ae09b28
Adesh Pandey <adesh@codebee.dk>
2026-05-12 17:00:23 +0545

    feat: add core entities and enums

---
+++ Backend/TaskFlowAPI/TaskFlow.Core/Entities/Enum/ProjectRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFlow.Core.Entities.Enum
+{
+    public enum ProjectRole
+    {
+        Admin,
+        Member
+    }
+}
---
+++ Backend/TaskFlowAPI/TaskFlow.Core/Entities/TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using TaskFlow.Core.Entities.Enum;
+
+namespace TaskFlow.Core.Entities
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public TaskItemStatus Status { get; set; }
+
+        public Guid ProjectId { get; set; }
+
+        public Guid? AssignedUserId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
